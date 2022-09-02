6ddff7a90c3adbf9629d02e5020ff4160327cfd5
kadjitaa <k.asumbisa@gmail.com>
2022-09-02 06:58:53 -0400

    edits

---
+++ HeadDirectionCells/Asumbisa_et_al_2022/main_script.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 17:36:19 2022
+
+@author: kasum
+"""
+from wrappers import *
+from functions import *
+import glob, os 
+
+###############################################################
+# PARAMETERS
+###############################################################
+data_directory=r'D:\Asumbisa_et_al_22_Dataset\floorRot_sessions\wt\KA84-201128' #session directory
+all_files = glob.glob(os.path.join(data_directory, "*.csv"))
+
+#Find all epochs in Epoch_TS.csv
+eps=[]
+for i in range(len(all_files)):
+    if all_files[i].split('_')[-1].split('.')[0] != 'TS':
+        eps.append(int(all_files[i].split('_')[-1].split('.')[0]))
+
+if 0 in eps:
+    episodes=['wake']*len(eps)
+else:
+    episodes=['sleep']+(['wake']*len(eps))
+
+#Generate index of wake epochs        
+events=[i for i,x in enumerate(episodes) if x!='sleep']    
+
+#Extract spikes, position data and wake epochs
+n_analogin_channels = 2  #number of analogin channels open
+channel_optitrack=1 #index of TTL channel
+spikes,shank= loadSpikeData(data_directory) #loads spikes, and probe shank index
+n_channels, fs, shank_to_channel = loadXML(data_directory)  #shank to channel 
+position= loadPosition(data_directory,events,episodes,n_analogin_channels,channel_optitrack) #loads position
+wake_ep=loadEpoch(data_directory,'wake',episodes) #loads time intervals for wake trial epochs
+
+##################################################################
+# PLOT: Tuning curves by trial
+##################################################################
+cols=int((len(spikes.keys()))/4)+1
+for i in range(len(events)):
+    ep=nts.IntervalSet(start=wake_ep.loc[i,'start'], end=wake_ep.loc[i,'end'])
+    tc=computeAngularTuningCurves(spikes,position['ry'],ep,60)
+    plt.figure()
+    plt.suptitle('Trial '+ str(i+1))
+    for x in spikes.keys():
+        subplot(4,cols,1+x, projection='polar')
+        plot(tc[x],label=str(x))
+    legend()
